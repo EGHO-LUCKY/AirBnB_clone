ea7abd77a485513a43d96f0e5ebde1d5413ee8ab
EGHO-LUCKY <egholucky8@gmail.com>
2023-12-10 22:01:13 +0000

    Add docstring to file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,13 +10,16 @@ class FileStorage:
     __objects = {}
 
     def all(self):
+        """Returns the dictionary of objects"""
         return self.__objects
 
     def new(self, obj):
+        """Adds a new object to the object dictionary"""
         obj_class_name = obj.__class__.__name__ + "." + obj.id
         self.__objects[obj_class_name] = obj
 
     def save(self):
+        """Saves the object dictionary to a file"""
         with open(self.__file_path, 'w', encoding="UTF-8") as file:
             new_dict = {}
             for key, value in self.__objects.items():
@@ -24,6 +27,7 @@ class FileStorage:
             json.dump(new_dict, file)
 
     def reload(self):
+        """Loads the file into the object dictionary"""
         try:
             from models.base_model import BaseModel
             with open(self.__file_path, 'r', encoding="UTF-8") as file:
