a1ea77c06b64665a98eaeae921ff7c24c958572d
EGHO-LUCKY <egholucky8@gmail.com>
2023-12-13 00:24:45 +0000

    Add user.py

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is the module of the User class"""
+
+import models
+
+class User(models.base_model.BaseModel):
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
